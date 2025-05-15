ef70abfbd09562e05831c479512c55f291515365
ameliapham <ptt.huong.8895@gmail.com>
2025-05-15 02:44:23 +0200

    Create first house

--- src/script.tsx
+++ src/script.tsx
@@ -16,26 +16,68 @@ const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper)
 
 // --- Objects ---
-// Sphere 
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({
-        roughness : 0.5
-    })
+// Ground
+const ground = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20),
+    new THREE.MeshStandardMaterial()
 )
-scene.add(sphere)
+ground.rotation.x = - Math.PI/2
+scene.add(ground)
+
+// House container
+const house = new THREE.Group()
+scene.add(house)
+
+// First Floor
+const firstFloor = new THREE.Mesh(
+    new THREE.CylinderGeometry(1.5, 1.3, 4, 4, 1),
+    new THREE.MeshStandardMaterial()
+)
+firstFloor.rotation.y = Math.PI/4
+firstFloor.position.y = 1.25
+house.add(firstFloor)
+
+// Second Floor
+const secondFloor = new THREE.Mesh(
+    new THREE.CylinderGeometry(1.2, 1.2, 2.1, 3, 1),
+    new THREE.MeshStandardMaterial()
+)
+secondFloor.position.y = 3.8
+secondFloor.rotation.x = - Math.PI/2
+house.add(secondFloor)
+
+// Roofs
+const roofs = new THREE.Group()
+const roofLeft = new THREE.Mesh(
+    new THREE.BoxGeometry(2.5, 3, 0.1),
+    new THREE.MeshStandardMaterial()
+)
+roofLeft.position.set(0, 3.8, -0.75)
+roofLeft.rotation.x = Math.PI * 0.168
+
+const roofRight = new THREE.Mesh(
+    new THREE.BoxGeometry(2.5, 3, 0.1),
+    new THREE.MeshStandardMaterial()
+)
+roofRight.position.set(0, 3.8, 0.75)
+roofRight.rotation.x = - Math.PI * 0.168
+
+roofs.add(roofLeft, roofRight)
+roofs.rotation.y = Math.PI/2
+house.add(roofs)
 
 // --- Lights ---
 const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
-directionalLight.position.set(2, 2, 0)
+const directionalLight = new THREE.DirectionalLight(0xfffffc, 1)
+directionalLight.position.set(2, 10, 0)
 
 scene.add(ambientLight, directionalLight)
 
 // --- Camera Setup ---
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight);
-camera.position.z = 5
+camera.position.set(4, 10, 0)
+camera.lookAt(house.position)
 scene.add(camera)
 
 // --- Controls ---
