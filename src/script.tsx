685f7789551f6704668ea1774bda13cf69068e58
ameliapham <ptt.huong.8895@gmail.com>
2025-05-17 16:53:55 +0200

    Add ground floor texture

--- src/script.tsx
+++ src/script.tsx
@@ -50,15 +50,15 @@ const firstFloorNormalTexture = textureLoader.load('public/texture/wall/green_ro
 
 firstFloorColorTexture.colorSpace = THREE.SRGBColorSpace
 
-firstFloorColorTexture.repeat.set(5, 4)
+firstFloorColorTexture.repeat.set(6, 5)
 firstFloorColorTexture.wrapS = THREE.RepeatWrapping
 firstFloorColorTexture.wrapT = THREE.RepeatWrapping
 
-firstFloorARMTexture.repeat.set(5, 4)
+firstFloorARMTexture.repeat.set(6, 5)
 firstFloorARMTexture.wrapS = THREE.RepeatWrapping
 firstFloorARMTexture.wrapT = THREE.RepeatWrapping
 
-firstFloorNormalTexture.repeat.set(5, 4)
+firstFloorNormalTexture.repeat.set(6, 5)
 firstFloorNormalTexture.wrapS = THREE.RepeatWrapping
 firstFloorNormalTexture.wrapT = THREE.RepeatWrapping
 
@@ -71,21 +71,38 @@ secondFloorColorTexture.colorSpace = THREE.SRGBColorSpace
 
 secondFloorColorTexture.center.set(0.5, 0.5)
 secondFloorColorTexture.rotation = Math.PI /2
-secondFloorColorTexture.repeat.set(3, 3)
-secondFloorColorTexture.wrapS = THREE.RepeatWrapping
-secondFloorColorTexture.wrapT = THREE.RepeatWrapping
+secondFloorColorTexture.repeat.set(4, 3.5)
+secondFloorColorTexture.wrapS = secondFloorColorTexture.wrapT = THREE.RepeatWrapping
 
 secondFloorARMTexture.center.set(0.5, 0.5)
 secondFloorARMTexture.rotation = Math.PI /2
-secondFloorARMTexture.repeat.set(3, 3)
-secondFloorARMTexture.wrapS = THREE.RepeatWrapping
-secondFloorARMTexture.wrapT = THREE.RepeatWrapping
+secondFloorARMTexture.repeat.set(4, 3.5)
+secondFloorARMTexture.wrapS = secondFloorARMTexture.wrapT = THREE.RepeatWrapping
 
 secondFloorNormalTexture.center.set(0.5, 0.5)
 secondFloorNormalTexture.rotation = Math.PI /2
-secondFloorNormalTexture.repeat.set(3, 3)
-secondFloorNormalTexture.wrapS = THREE.RepeatWrapping
-secondFloorNormalTexture.wrapT = THREE.RepeatWrapping
+secondFloorNormalTexture.repeat.set(4, 3.5)
+secondFloorNormalTexture.wrapS = secondFloorNormalTexture.wrapT = THREE.RepeatWrapping
+
+// Ground Floor
+const groundFloorColorTexture = textureLoader.load('public/texture/wall/wood_planks_grey_1k/wood_planks_grey_diff_1k.jpg')
+const groundFloorARMTexture = textureLoader.load('public/texture/wall/wood_planks_grey_1k/wood_planks_grey_arm_1k.jpg')
+const groundFloorNormalTexture = textureLoader.load('public/texture/wall/wood_planks_grey_1k/wood_planks_grey_nor_gl_1k.jpg')
+
+groundFloorColorTexture.colorSpace = THREE.SRGBColorSpace
+
+groundFloorColorTexture.center.set(0.5, 0.5)
+groundFloorColorTexture.rotation = Math.PI /2
+
+groundFloorARMTexture.center.set(0.5, 0.5)
+groundFloorColorTexture.rotation = Math.PI /2
+
+groundFloorARMTexture.center.set(0.5, 0.5)
+groundFloorColorTexture.rotation = Math.PI /2
+
+groundFloorColorTexture.wrapS = groundFloorColorTexture.wrapT = THREE.RepeatWrapping
+groundFloorARMTexture.wrapS = groundFloorARMTexture.wrapT = THREE.RepeatWrapping
+groundFloorNormalTexture.wrapS = groundFloorNormalTexture.wrapT = THREE.RepeatWrapping
 
 // --- Objects ---
 // Platform ---------------------------
@@ -115,39 +132,68 @@ scene.add(house)
 const groundFloor = new THREE.Group()
 house.add(groundFloor)
 
+const groundFloorMaterial = new THREE.MeshStandardMaterial({
+    map : groundFloorColorTexture,
+    aoMap : groundFloorARMTexture,
+    roughnessMap : groundFloorARMTexture,
+    metalnessMap : groundFloorARMTexture,
+    normalMap : groundFloorNormalTexture
+})
+
 const foundation = new THREE.Mesh(
     new THREE.BoxGeometry(2, 0.9, 2),
-    new THREE.MeshStandardMaterial()
+    groundFloorMaterial
 )
 foundation.position.y = 0.45
 groundFloor.add(foundation)
 
 // Steps
-const stepMaterial = new THREE.MeshStandardMaterial()
+function createStep(width: number, height: number, depth : number, position : [number, number, number]){
+    const color = groundFloorColorTexture
+    const arm = groundFloorARMTexture
+    const normal = groundFloorNormalTexture
+
+    color.wrapS = color.wrapT = THREE.RepeatWrapping
+    arm.wrapS = arm.wrapT = THREE.RepeatWrapping
+    normal.wrapS = normal.wrapT = THREE.RepeatWrapping
+
+    color.center.set(0.5, 0.5)
+    arm.center.set(0.5, 0.5)
+    normal.center.set(0.5, 0.5)
+
+    color.rotation = Math.PI / 2
+    arm.rotation = Math.PI / 2
+    normal.rotation = Math.PI / 2
+
+    color.repeat.set(width, depth)
+    arm.repeat.set(width, depth)
+    normal.repeat.set(width, depth)
+
+    const material = new THREE.MeshStandardMaterial({
+        map: color,
+        aoMap: arm,
+        roughnessMap: arm,
+        metalnessMap: arm,
+        normalMap: normal
+    })
+
+    const step = new THREE.Mesh(
+        new THREE.BoxGeometry(width, height, depth),
+        material
+    )
+    step.position.set(...position)
+    return step
+}
 
 const entrySteps = new THREE.Group()
 groundFloor.add(entrySteps)
 entrySteps.position.y = 0.15
 
-const firstStep = new THREE.Mesh(
-    new THREE.BoxGeometry(2, 0.3, 1),
-    stepMaterial
-)
-firstStep.position.set (0, 0, 1.5)
-entrySteps.add(firstStep)
+const firstStep = createStep(2, 0.3, 1, [0, 0, 1.5])
+const secondStep = createStep(1.7, 0.3, 0.8, [0, 0.3, 1.4])
+const thirdStep = createStep(1.4, 0.3, 0.6, [0, 0.6, 1.3])
 
-const secondStep = new THREE.Mesh(
-    new THREE.BoxGeometry(1.7, 0.3, 0.8),
-    stepMaterial)
-secondStep.position.set (0, 0.3, 1.4)
-entrySteps.add(secondStep)
-
-const thirdStep = new THREE.Mesh(
-    new THREE.BoxGeometry(1.4, 0.3, 0.6),
-    stepMaterial
-)
-thirdStep.position.set (0, 0.6, 1.3)
-entrySteps.add(thirdStep)
+entrySteps.add(firstStep, secondStep, thirdStep)
 
 // First Floor
 const firstFloor = new THREE.Mesh(
