6266344462ffc83ba07c892b2f87ad19e5514426
ameliapham <ptt.huong.8895@gmail.com>
2025-05-21 00:44:54 +0200

    Ghosts and its animation

--- src/script.tsx
+++ src/script.tsx
@@ -457,10 +457,17 @@ for(let i = 0; i < 30; i++){
     graves.add(grave)
 }
 
+// Ghosts ------------------------------------------
+const ghost1 = new THREE.PointLight(0x2d00f7, 6)
+const ghost2 = new THREE.PointLight(0x45B944, 6)
+const ghost3 = new THREE.PointLight(0x6a00f4, 6)
+
+scene.add(ghost1, ghost2, ghost3)
+
 // --- Lights ---
 const ambientLight = new THREE.AmbientLight(0x86cdff, 0.275)
 
-const directionalLight = new THREE.DirectionalLight(0x86cdff, 1)
+const directionalLight = new THREE.DirectionalLight(0x86cdff, 2)
 directionalLight.position.set(4, 8, -5)
 
 const rectAreaLightWindow = new THREE.RectAreaLight(0xffffff, 4, 0.2, 0.3)
@@ -507,6 +514,23 @@ function animate(){
     // Timer
     timer.update()
     const elapsedTime = timer.getElapsed()
+
+    // Ghosts Animation
+    const ghost1Angle = elapsedTime / 4
+    ghost1.position.x = Math.cos(ghost1Angle) * 3
+    ghost1.position.z = Math.sin(ghost1Angle) * 5
+    ghost1.position.y = Math.sin(ghost1Angle) * Math.sin(ghost1Angle * 2.34) * Math.sin(ghost1Angle * 4.56) 
+
+    const ghost2Angle = - elapsedTime / 2
+    ghost2.position.x = Math.cos(ghost2Angle) * 2
+    ghost2.position.z = Math.sin(ghost2Angle) * 2
+    ghost2.position.y = Math.sin(ghost2Angle) * Math.sin(ghost2Angle * 2.34) * Math.sin(ghost2Angle * 3.45) 
+
+    const ghost3Angle = - elapsedTime / 6
+    ghost3.position.x = Math.cos(ghost3Angle) * 6
+    ghost3.position.z = Math.sin(ghost3Angle) * 6
+    ghost3.position.y = Math.sin(ghost3Angle) * Math.sin(ghost3Angle * 2.34) * Math.sin(ghost3Angle * 3.45) 
+
 
     // Update control
     controls.update()
